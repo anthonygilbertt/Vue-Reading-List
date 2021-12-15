2ce2781eb23cc9148f82a3550db3dcd1da735710
anthonygilbert <anthony.gilbert93@gmail.com>
2021-12-15 15:50:26 -0600

    added firebase config and imported firebase data

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCWrx0lidSj5ugsS7Z7Ct4Tvxcrg2F4ehY",
+  authDomain: "fir-9-reading-list-b2a53.firebaseapp.com",
+  projectId: "fir-9-reading-list-b2a53",
+  storageBucket: "fir-9-reading-list-b2a53.appspot.com",
+  messagingSenderId: "325687782751",
+  appId: "1:325687782751:web:31131a4b530f0177801aea",
+};
+
+// Init Firebase
+initializeApp(firebaseConfig);
+
+// Init Services
+const db = getFirestore();
+
+export { db };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,21 +16,32 @@
 </template>
 
 <script>
-import { ref } from 'vue'
-import CreateBookForm from '@/components/CreateBookForm'
+import { ref } from "vue";
+import CreateBookForm from "@/components/CreateBookForm";
+
+// Firebase imports
+import { db } from "@/firebase/config";
+import { collection, getDocs } from "firebase/firestore";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: { CreateBookForm },
   setup() {
-    const books = ref([
-      { title: 'name of the wind', author: 'patrick rothfuss', isFav: false, id: '1' },
-      { title: 'the way of kings', author: 'brandon sanderson', isfav: false, id: '2' }
-    ])
+    const books = ref([]);
+    const collectionReference = collection(db, "books");
 
-    return { books }
-  }
-}
+    getDocs(collectionReference).then((snapshot) => {
+      let docs = [];
+      snapshot.docs.forEach((doc) => {
+        // get data and the id
+        docs.push({ ...doc.data(), id: doc.id });
+      });
+      books.value = docs;
+    });
+
+    return { books };
+  },
+};
 </script>
 
 <style>
